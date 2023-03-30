f53ba897e188c98e40a579347083699a2c8fdd52
Emiliano <grimaldi385@gmail.com>
2023-03-29 21:00:58 -0300

    Notebook

--- Python Prog 1/Ejercicios.py
+++ Python Prog 1/Ejercicios.py
@@ -18,23 +18,26 @@
 continuar = 1
 acumulador_bebida = 0
 acumulador_comida = 0
-cerveza = 500
-limonada = 300
-gaseosa = 250
-papitas = 1200
-hamburguesa = 2000
-rabas = 1800
-bandera_bebidas = 1
-bandera_comidas = 1
+acumulador_precio_cerveza = 0
+acumulador_precio_limonada = 0
+acumulador_precio_gaseosa = 0
+acumulador_precio_papitas = 0
+acumulador_precio_hamburguesa = 0
+acumulador_precio_rabas = 0
+bandera_bebidas = True
+bandera_comidas = True
 acumulador_cervezas = 0
-acumulador_limonada = 0
-acumulador_gaseosa = 0
-acumulador_papita = 0
-acumulador_hamburguesa = 0
+acumulador_limonadas = 0
+acumulador_gaseosas = 0
+acumulador_papitas = 0
+acumulador_hamburguesas = 0
 acumulador_rabas = 0
+contador_de_clientes = 0
+promedio_de_bebidas = None
+recaudacion_bruta = None
+promedio_de_comida = None
 
-
-while(continuar == 1):
+while (continuar == 1):
 
     nombre_cliente = input("Ingresar el nombre: ")
 
@@ -49,43 +52,71 @@ while(continuar == 1):
     while (tipo_de_comida != "papitas" and tipo_de_comida != "hamburguesa" and tipo_de_comida != "rabas" and tipo_de_comida != "nada"):
         tipo_de_comida = input(
             "Reingresar el tipo de comida[papitas - hamburguesa - rabas - nada]: ")
-        
+
     match(tipo_de_bebida):
         case "cerveza":
             acumulador_bebida += 1
-            bandera_bebidas = 0
             acumulador_cervezas += 1
+            acumulador_precio_cerveza += 500
+            bandera_bebidas = False
         case "limonada":
             acumulador_bebida += 1
-            bandera_bebidas = 0
-            acumulador_limonada += 1
+            acumulador_limonadas += 1
+            acumulador_precio_limonada += 300
+            bandera_bebidas = False
         case "gaseosa":
             acumulador_bebida += 1
-            bandera_bebidas = 0
-            acumulador_gaseosa += 1
+            acumulador_gaseosas += 1
+            acumulador_precio_gaseosa += 250
+            bandera_bebidas = False
 
     match(tipo_de_comida):
         case "papitas":
             acumulador_comida += 1
-            bandera_comidas = 0
+            acumulador_papitas += 1
+            acumulador_precio_papitas += 1200
+            bandera_comidas = False
         case "hamburguesa":
             acumulador_comida += 1
-            bandera_comidas = 0
+            acumulador_hamburguesas += 1
+            acumulador_precio_hamburguesa += 2000
+            bandera_comidas = False
         case "rabas":
             acumulador_comida += 1
-            bandera_comidas = 0
-# a.	El tipo de comida más vendido y bebida más vendida si la hay.
-# b.	El promedio de clientes que ordena solamente bebida.
-# c.	Calcular la recaudación bruta y recaudación neta del local.
-# d.	Cuánta gente ordenó comida pero no bebida.
-    if (bandera_bebidas == 0):
-        print("-")
-    else:
-        print("No se ingresaron bebidas")
+            acumulador_rabas += 1
+            acumulador_precio_rabas += 1800
+            bandera_comidas = False
 
-    if (bandera_comidas == 0):
-        print("Se ingresó comida")
-    else:
-        print("No se ingresó comida")
-    
+    contador_de_clientes += 1
     continuar = int(input("Para continuar presione 1: "))
+
+# a.	El tipo de comida más vendido y bebida más vendida si la hay.
+if (bandera_bebidas == False):
+    if (acumulador_cervezas > acumulador_gaseosas and acumulador_cervezas > acumulador_limonadas):
+        print("La cerveza es la bebida mas vendida")
+    else:
+        if (acumulador_gaseosas > acumulador_cervezas and acumulador_gaseosas > acumulador_limonadas):
+            print("La gaseosa es la bebida mas vendida")
+        else:
+            print("La limonada es la bebida mas vendida")
+else:
+    print("No se ingresaron bebidas")
+
+if (bandera_comidas == False):
+    if (acumulador_papitas > acumulador_hamburguesas and acumulador_papitas > acumulador_rabas):
+        print("Las papitas es la comida mas vendida")
+    else:
+        if (acumulador_hamburguesas > acumulador_papitas and acumulador_hamburguesas > acumulador_rabas):
+            print("La hamburguesa es la comida mas vendida")
+        else:
+            print("Las rabas es la comida mas vendida")
+else:
+    print("No se ingresó comida")
+# b.	El promedio de clientes que ordena solamente bebida.
+
+# c.	Calcular la recaudación bruta y recaudación neta del local.
+recaudacion_bruta = acumulador_precio_cerveza + acumulador_precio_gaseosa + acumulador_precio_limonada + acumulador_precio_hamburguesa + acumulador_precio_papitas + acumulador_precio_rabas
+print("La recaudacion bruta del local es: ",recaudacion_bruta,"$")
+# Falta calcular la recaudacion neta del local
+
+# d.	Cuánta gente ordenó comida pero no bebida.
