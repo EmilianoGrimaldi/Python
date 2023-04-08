4a9a338c421042a044183cb358a6af167a76928f
Emiliano <grimaldi385@gmail.com>
2023-04-08 15:36:55 -0300

    PC

--- Ejercicios/Listas_02.py
+++ Ejercicios/Listas_02.py
@@ -1,27 +1,47 @@
 #La real academia española nos pide desarrollar un pequeño programa que contenta el diccionario de la lengua española y su traducción al inglés. Para esto necesitamos un algoritmo que nos permita el ingreso de una palabra en español y su traducción al ingles y guardarlo en una lista. Si la palabra no existe, debemos agregarla, y si la palabra existe debemos informar que la palabra ya existe y su índice dentro del listado, esta carga debe ser hasta que el usuario se canse de ingresar palabras. Al finalizar se debe mostrar todo el listado de palabras ingresadas con su palabra equivalente en inglés. Recordar validar los datos de forma correcta.
+# inicializamos las listas vacías
 seguir = "s"
-lista_palabras = []
+palabras_espanol = []
+palabras_ingles = []
+
 while seguir == "s":
-    while True:
-        palabra_espanol = input("Ingrese una palabra en español: ")
-        if palabra_espanol.isalpha() :
+    # INGRESO DE DATOS Y VALIDACIONES
+    while True: 
+        palabra_espanol = input("Ingrese una palabra en español: ").lower()
+        if palabra_espanol.isalpha():
             break
         else:
-            print("ERROR! Se permiten solo palabras")
+            print("ERROR! Se permiten solo palabras. Ingrese una palabra en español.")
 
-    while True:
-        palabra_ingles = input("Ingrese la traduccion al ingles: ")
-        if palabra_ingles.isalpha() :
+    while True :
+        palabra_ingles = input("Ingrese la traducción al inglés: ").lower()
+        if palabra_ingles.isalpha():
             break
         else:
-            print("ERROR! Se permiten solo palabras")
-    
-    while True:
-        seguir = input("'s' para continuar y 'n' para salir\n")
-        if(seguir == "s" or seguir == "n"):
-            break
-        else:
-            print("ERROR! Opcion incorrecta.")
+            print("ERROR! Se permiten solo palabras. Ingrese la traducción al inglés: ").lower()
 
-# for palabras in lista_palabras:
-#     print(f"Palabra Español: {lista_palabras[0][palabras]} Palabra Ingles: {lista_palabras[1][palabras]}")
+    # buscamos si la palabra ya existe en la lista de palabras en español
+    palabra_encontrada = False
+    for i in range(len(palabras_espanol)):
+        if palabras_espanol[i] == palabra_espanol:
+            palabra_encontrada = True
+            indice_palabra = i
+            break
+
+
+    # si la palabra ya existe, informamos su índice en la lista
+    if palabra_encontrada == True:
+        print(f"La palabra '{palabra_espanol}' ya existe en la lista, en la posición {indice_palabra}.")
+    # si la palabra no existe, la agregamos a ambas listas
+    else:
+        palabras_espanol.append(palabra_espanol)
+        palabras_ingles.append(palabra_ingles)
+        print(f"La palabra '{palabra_espanol}' ha sido agregada a la lista.")
+
+    # PREGUNTAR SI DESEA CONTINUAR
+    seguir = input("Presione 's' para continuar o cualquier tecla para salir: ").lower()
+
+# mostramos la lista de palabras ingresadas
+print("Lista de palabras ingresadas:")
+for i in range(len(palabras_espanol)):
+    print(f"- '{palabras_espanol[i]}' en inglés es '{palabras_ingles[i]}'")
--- Ejercicios/Listas_04.py
+++ Ejercicios/Listas_04.py
@@ -8,25 +8,88 @@
 # o Cantidad y porcentaje de votos negativos.
 # o Cantidad y porcentaje de abstenciones.
 # o Generar una lista de senadores por cada tipo de voto y mostrarlas por pantalla.
+continuar = "s"
+senadores_totales = 0
+senadores_presentes = 0
+cantidad_senadores_afirmativos = 0
+cantidad_senadores_negativos = 0
+cantidad_senadores_abstenciones = 0
+lista_votos_afirmativos = []
+lista_votos_negativos = []
+lista_votos_abstenciones = []
 
-#Pedir en pantalla el nombre de cada senador
+while continuar == "s":
+    #Pedir en pantalla el nombre de cada senador
+    while True:
+        nombre_senador = input("Ingrese el nombre del senador\n")
+        if nombre_senador.isalpha():
+            break
+        else:
+            print("Error! Solo se permiten caracteres alfabeticos")
 
-#Verificar si esta presente o no en la sesion
-# si (esta presente):
-    #pido el valor del voto
-    # afirmativo o negativo
-    #cantidad de senadores presentes
-    # si (afirmativo):
+    while True:
+        presencia_senador = input("Ingrese si esta [presente - ausente]\n").lower()
+        if presencia_senador == "presente" or presencia_senador == "ausente":
+            break
+        else:
+            print("Error! Ingreso una opcion inexistente") 
+
+    #Verificar si esta presente o no en la sesion
+    # si (esta presente):
+    if presencia_senador == "presente":
+        #pido el valor del voto
+        while True:
+            voto_senador = input("Ingrese el valor del voto [afirmativo - negativo]\n").lower()
+            if voto_senador == "afirmativo" or voto_senador == "negativo":
+                break
+            else:
+                print("Error! Ingreso una opcion inexistente") 
+        #cantidad de senadores presentes
+        senadores_presentes += 1
+        # si (afirmativo):
+        if voto_senador == "afirmativo":
+            #cuento la cantidad y calculo el porcentaje
+            cantidad_senadores_afirmativos += 1
+            lista_votos_afirmativos.append(nombre_senador)
+        # si (negativo):
+        else:
+            if voto_senador == "negativo":
+                #cuento la cantidad y calculo el porcentaje
+                cantidad_senadores_negativos += 1
+                lista_votos_negativos.append(nombre_senador)
+    # sino:
+    else:
+        #el voto sera abstencion
+        voto_senador = "abstencion"
+        cantidad_senadores_abstenciones += 1
+        lista_votos_abstenciones.append(nombre_senador)
         #cuento la cantidad y calculo el porcentaje
-    # si (negativo):
-        #cuento la cantidad y calculo el porcentaje
-# sino:
-    #el voto sera abstencion
-    #cuento la cantidad y calculo el porcentaje
-# cuento el total de senadores
-# Necesito una lista de senadores totales
-# Senadores presentes
-# senadores ausentes
-# senadores votos afirmativos
-# senadores votos negativos
-# senadores votos abstencion
+    
+    continuar = input("Para continuar ingrese 's'. Para salir cualquier tecla\n" )
+#calculos
+senadores_totales = cantidad_senadores_afirmativos + cantidad_senadores_negativos + cantidad_senadores_abstenciones
+porcentaje_votos_afirmativos = (cantidad_senadores_afirmativos * 100)/senadores_totales
+porcentaje_votos_negativos = (cantidad_senadores_negativos * 100)/senadores_totales
+porcentaje_votos_abstenciones = (cantidad_senadores_abstenciones * 100)/senadores_totales
+#informes
+
+# o Cantidad total de senadores.
+print(f"Cantidad total de senadores: {senadores_totales}")
+# o Cantidad de senadores presentes.
+print(f"Cantidad de senadores presentes: {senadores_presentes}")
+# o Cantidad y porcentaje de votos afirmativos.
+print(f"Cantidad votos afirmativos: {cantidad_senadores_afirmativos}. Porcentaje de votos afirmativos: %{porcentaje_votos_afirmativos}")
+# o Cantidad y porcentaje de votos negativos.
+print(f"Cantidad votos negativos: {cantidad_senadores_negativos}. Porcentaje de votos negativos: %{porcentaje_votos_negativos}")
+# o Cantidad y porcentaje de abstenciones.
+print(f"Cantidad de abstenciones: {cantidad_senadores_abstenciones}. Porcentaje de abstenciones: %{porcentaje_votos_abstenciones}")
+# o Generar una lista de senadores por cada tipo de voto y mostrarlas por pantalla.
+print("######### LISTA SENADORES VOTOS AFIRMATIVOS #############")
+for senador in lista_votos_afirmativos:
+    print(senador)
+print("######### LISTA SENADORES VOTOS NEGATIVOS #############")
+for senador in lista_votos_negativos:
+    print(senador)
+print("######### LISTA SENADORES ABSTENCIONES #############")
+for senador in lista_votos_abstenciones:
+    print(senador)
