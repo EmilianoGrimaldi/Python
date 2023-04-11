c9ca09033594bbc12288cc076dad3479bee831c5
Emiliano <grimaldi385@gmail.com>
2023-04-11 03:59:54 -0300

    PC

--- Ejercicios/Listas_08.py
+++ Ejercicios/Listas_08.py
@@ -7,3 +7,36 @@
 #PASO 4 si esta presente el programa deberá imprimir la posición del nombre en la lista si no se encuentra, se deberá informar por pantalla.
 #PASO 5 la posición de memoria donde se encuentra ese nombre
 #PASO 6 la cantidad de caracteres que tiene el nombre
+# lista_nombres = [Emiliano Federico Julieta Noelia Gisel Teresa Omar Carlos Rodrigo]
+flag_posicion = False
+flag_nombre_valido = False
+
+while True:
+
+    while True:
+        lista_nombres = input("Ingrese una lista de nombres separados por espacios\n").split()
+        for nombre in lista_nombres:
+            for caracter in nombre:
+                pass
+
+    nombre_ingresado = input("Ingrese un nombre a buscar\n")
+    for nombre in lista_nombres:
+        if nombre == nombre_ingresado:
+            posicion = lista_nombres.index(nombre)
+            id_nombre = id(nombre)
+            tam_nombre = len(nombre)
+            flag_posicion = True
+
+    if flag_posicion:
+        print(f"La posicion del nombre en la lista es {posicion}")
+        print(f"El nombre se encuentra en la posicion de memoria {id_nombre}")
+        print(f"El tamaño del nombre es {tam_nombre}")
+    else:
+        print("No se encuentra ese nombre en la lista")
+
+    seguir = input("Desea[seguir? [s/n]\n")
+    if seguir != "s":
+        seguir = input("Esta seguro que quiere salir? [s/n]\n")
+        if seguir != "s":
+            break
+
