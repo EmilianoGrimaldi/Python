ab2b74a1f81184ab931c3a532d25cc36669cb97a
Emiliano <grimaldi385@gmail.com>
2023-04-13 01:15:51 -0300

    PC

--- Teoria Prog Lab/sarasa/main.py
+++ Teoria Prog Lab/sarasa/main.py
@@ -1,3 +1,20 @@
-# from data import *
+from data import *
 
-print(personas)
+flag_edad_maxima = True
+# datos_personas_mayor = []
+
+for persona in personas:
+    if flag_edad_maxima or persona["edad"] >= edad_maxima :
+        edad_maxima = persona["edad"]
+        datos_mayor_edad = persona
+        flag_edad_maxima = False
+
+print(edad_maxima)
+
+#para filtrar 
+for persona in personas:
+    if edad_maxima == persona["edad"]:
+        print(persona)
+        # datos_personas_mayor.append(persona)
+
+# print(datos_personas_mayor)
