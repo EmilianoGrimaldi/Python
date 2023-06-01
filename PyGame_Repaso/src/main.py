0886c0f59f8ed6bf0c3ee0dd2722e56762147369
Emiliano <grimaldi385@gmail.com>
2023-06-01 17:36:14 -0300

    Repasando

--- PyGame_Repaso/src/main.py
+++ PyGame_Repaso/src/main.py
@@ -1,1 +1,26 @@
-import pygame
+import pygame, sys
+
+ANCHO = 600
+ALTURA = 300
+TITULO = "Primer aplicacion"
+COLOR = (255,0,0)
+
+
+pygame.init()
+# print(pygame.get_init())
+
+resolucion = pygame.display.set_mode((ANCHO,ALTURA))
+pygame.display.set_caption(TITULO)
+# in_game = True
+
+while True:
+    
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            # in_game = False
+    resolucion.fill(COLOR)
+    
+    pygame.display.flip()
+    
